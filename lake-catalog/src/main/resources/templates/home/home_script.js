231cedb314ed89369575553b6e45e164148cb7fa
Olesya-Khulap <olesya.khulap@yandex.ru>
2024-11-07 18:26:25 +0300

    added home page

---
+++ lake-catalog/src/main/resources/templates/home/home_script.js
@@ -0,0 +1,11 @@
+function register() {
+    window.location.href = "../register/register.html";
+}
+
+function login() {
+    window.location.href = "../login/login.html";
+}
+
+function continueWithoutRegister() {
+    window.location.href = "../main/main.html";
+}
